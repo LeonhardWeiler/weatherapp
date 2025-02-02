d851a1c378ca20065b3355dbd69d578a6f35e1a7
weilerl <leonhard.weiler@edu.htl-villach.at>
2025-02-02 15:59:53 +0100

    noch nie so glücklich hihi

--- js/blueprint.js
+++ js/blueprint.js
@@ -6,7 +6,17 @@ const structure = `
         <img class="icon-heading img-current">
       </div>
       <div class="subtitle">
-        <p class="city name-current"></p>
+        <div class="which-city">
+          <p class="city name-current"></p>
+          <div class="change-city-icon">
+            <svg viewBox="0 -0.5 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M13.294 7.959c.259-.324.207-.796-.116-1.055s-.795-.208-1.055.115l1.171.94zM6.811 14.849l.568.49c.006-.006.012-.013.017-.02l-.585-.47zM6.64 15.267l-.748-.049-.001.014.75.035zM6.5 18.29l-.749-.035a.805.805 0 00.003.112l.746-.077zM7.287 18.977l.025.75c.05-.002.1-.009.148-.02l-.173-.73zM10.287 18.266l.173.73.012-.003-.185-.727zM10.672 18.022l.579.477.006-.008-.585-.469zM17.297 10.959c.259-.324.207-.796-.116-1.055s-.795-.208-1.055.115l1.171.94zM12.127 7.021c-.259.324-.207.796.116 1.055s.796.208 1.055-.115l-1.171-.94zM14.3 5.51l.585.469c.01-.012.02-.025.03-.038l-.615-.431zM15.929 5.19l.48-.576a.8.8 0 00-.075-.055l-.405.631zM18.166 7.052l.531-.53a.8.8 0 00-.051-.047l-.48.577zM18.503 7.873h.75v-.001l-.75.001zM18.157 8.69l-.525-.536c-.021.021-.041.043-.06.066l.585.47zM16.127 10.02c-.26.323-.208.795.115 1.055.324.259.796.208 1.055-.115l-1.17-.94zM13.454 7.379c-.061-.41-.443-.692-.853-.63-.41.062-.692.444-.631.854l1.484-.224zM16.813 11.233c.41-.056.698-.434.642-.844-.056-.41-.434-.698-.844-.643l.202 1.487zM12.124 7.021L6.226 14.38l1.17.939 5.898-7.359-1.17-.939zM6.243 14.359a1.6 1.6 0 00-.35.86l1.497.098a.792.792 0 00.01-.023l-1.157-.935zM5.891 15.232l-.14 3.023 1.498.07.14-3.023-1.498-.07zM5.754 18.367c.082.792.762 1.386 1.558 1.36l-.05-1.499c-.003 0-.005 0-.006 0a.2.2 0 01-.005-.002l-.006-.005a.2.2 0 01-.004-.006l-1.49.152zM7.46 19.707l3-0.711-.346-1.46-3 .711.346 1.46zM10.472 18.993c.305-.078.578-.251.779-.494l-1.157-.954c.002-.003.005-.005.009-.006l.369 1.454zM11.257 18.491l6.04-7.532-1.17-.939-6.04 7.532 1.17.939zM13.297 7.959l1.588-1.98-1.17-.938-1.588 1.98 1.17.938zM14.914 5.941c.139-.197.408-.25.611-.12l.81-1.263c-.88-.563-2.047-.334-2.648.52l1.227.863zM15.449 5.766l2.237 1.862.96-1.153-2.237-1.862-.96 1.153zM17.635 7.582c.076.076.118.18.117.287h1.5c.003-.508-.197-.996-.556-1.355l-.561 1.068zM17.753 7.869c-.001.107-.045.21-.121.285l1.05 1.072c.363-.355.568-.84.571-1.347l-1.5-.01zM17.572 8.22l-1.445 1.8 1.17.939 1.445-1.8-1.17-.939zM11.97 7.601c.35 2.331 2.507 3.949 4.843 3.632l-.202-1.486c-1.523.207-2.929-.848-3.157-2.368l-1.484.222z"
+                fill="#000000"
+              />
+            </svg>
+          </div>
+        </div>
         <p class="description desc-current"></p>
       </div>
     </header>
@@ -92,92 +102,8 @@ const structure = `
     </main>
     <footer class="container">
       <div class="other-city-grid">
-        <div class="city-preview-box">
-          <div class="title">
-            <h2 class="text-l city-1"></h2>
-            <p class="city-1-temp-current"></p>
-          </div>
-          <div class="preview">
-            <div class="city-preview">
-              <h3 class="day city-1-day-0"></h3>
-              <div class="min-tmp city-1-min-temp-current"></div>
-              <div class="line-tmp city-1-med-temp-current"></div>
-              <div class="max-tmp city-1-max-temp-current"></div>
-              <img class="icon city-1-img-current">
-            </div>
-            <div class="city-preview">
-              <h3 class="day city-1-day-1"></h3>
-              <div class="min-tmp city-1-min-temp-day-1"></div>
-              <div class="line-tmp city-1-med-temp-day-1"></div>
-              <div class="max-tmp city-1-max-temp-day-1"></div>
-              <img class="icon city-1-img-day-1">
-            </div>
-            <div class="city-preview">
-              <h3 class="day city-1-day-2"></h3>
-              <div class="min-tmp city-1-min-temp-day-2"></div>
-              <div class="line-tmp city-1-med-temp-day-2"></div>
-              <div class="max-tmp city-1-max-temp-day-2"></div>
-              <img class="icon city-1-img-day-2">
-            </div>
-          </div>
-        </div>
-        <div class="city-preview-box">
-          <div class="title">
-            <h2 class="text-l city-2"></h2>
-            <p class="city-2-temp-current"></p>
-          </div>
-          <div class="preview">
-            <div class="city-preview">
-              <h3 class="day city-2-day-0"></h3>
-              <div class="min-tmp city-2-min-temp-current"></div>
-              <div class="line-tmp city-2-med-temp-current"></div>
-              <div class="max-tmp city-2-max-temp-current"></div>
-              <img class="icon city-2-img-current">
-            </div>
-            <div class="city-preview">
-              <h3 class="day city-2-day-1"></h3>
-              <div class="min-tmp city-2-min-temp-day-1"></div>
-              <div class="line-tmp city-2-med-temp-day-1"></div>
-              <div class="max-tmp city-2-max-temp-day-1"></div>
-              <img class="icon city-2-img-day-1">
-            </div>
-            <div class="city-preview">
-              <h3 class="day city-2-day-2"></h3>
-              <div class="min-tmp city-2-min-temp-day-2"></div>
-              <div class="line-tmp city-2-med-temp-day-2"></div>
-              <div class="max-tmp city-2-max-temp-day-2"></div>
-              <img class="icon city-2-img-day-2">
-            </div>
-          </div>
-        </div>
-        <div class="city-preview-box">
-          <div class="title">
-            <h2 class="text-l city-3"></h2>
-            <p class="city-3-temp-current"></p>
-          </div>
-          <div class="preview">
-            <div class="city-preview">
-              <h3 class="day city-3-day-0"></h3>
-              <div class="min-tmp city-3-min-temp-current"></div>
-              <div class="line-tmp city-3-med-temp-current"></div>
-              <div class="max-tmp city-3-max-temp-current"></div>
-              <img class="icon city-3-img-current">
-            </div>
-            <div class="city-preview">
-              <h3 class="day city-3-day-1"></h3>
-              <div class="min-tmp city-3-min-temp-day-1"></div>
-              <div class="line-tmp city-3-med-temp-day-1"></div>
-              <div class="max-tmp city-3-max-temp-day-1"></div>
-              <img class="icon city-3-img-day-1">
-            </div>
-            <div class="city-preview">
-              <h3 class="day city-3-day-2"></h3>
-              <div class="min-tmp city-3-min-temp-day-2"></div>
-              <div class="line-tmp city-3-med-temp-day-2"></div>
-              <div class="max-tmp city-3-max-temp-day-2"></div>
-              <img class="icon city-3-img-day-2">
-            </div>
-          </div>
+        <div class="add-city-box">
+          <div class="plus-icon"></div>
         </div>
       </div>
     </footer>
@@ -185,4 +111,43 @@ const structure = `
 
 const body = document.querySelector('body');
 body.innerHTML = structure;
+}
+
+export function addCity(id) {
+  const smallCity = `
+          <div class="title">
+            <h2 class="text-l city-${id}"></h2>
+            <p class="city-${id}-temp-current"></p>
+          </div>
+          <div class="preview">
+            <div class="city-preview">
+              <h3 class="day city-${id}-day-0"></h3>
+              <div class="min-tmp city-${id}-min-temp-current"></div>
+              <div class="line-tmp city-${id}-med-temp-current"></div>
+              <div class="max-tmp city-${id}-max-temp-current"></div>
+              <img class="icon city-${id}-img-current">
+            </div>
+            <div class="city-preview">
+              <h3 class="day city-${id}-day-1"></h3>
+              <div class="min-tmp city-${id}-min-temp-day-1"></div>
+              <div class="line-tmp city-${id}-med-temp-day-1"></div>
+              <div class="max-tmp city-${id}-max-temp-day-1"></div>
+              <img class="icon city-${id}-img-day-1">
+            </div>
+            <div class="city-preview">
+              <h3 class="day city-${id}-day-2"></h3>
+              <div class="min-tmp city-${id}-min-temp-day-2"></div>
+              <div class="line-tmp city-${id}-med-temp-day-2"></div>
+              <div class="max-tmp city-${id}-max-temp-day-2"></div>
+              <img class="icon city-${id}-img-day-2">
+            </div>
+          </div>
+  `;
+
+  const otherCityGrid = document.querySelector('.other-city-grid');
+  const div = document.createElement('div');
+  div.classList.add('city-preview-box')
+  div.innerHTML = smallCity;
+  otherCityGrid.insertBefore(div, otherCityGrid.firstChild);
+
 }
--- js/main.js
+++ js/main.js
@@ -1,288 +1,81 @@
-const API_KEY = "2fa73590fd8b5a4c6e68098ad5625395";
+import { createStructure } from "./blueprint.js";
+import { fetchCurrentCity } from "./weather.js";
 
-function formatTime(unixTimestamp) {
-  const date = new Date(unixTimestamp * 1000);
-  const hours = Math.floor(date.getHours());
-  return `${hours < 10 ? '0' : ''}${hours}`;
+const searchInput = document.querySelector(".search-main");
+const suggestionsContainer = document.querySelector(".suggestions");
+const apiKey = "2fa73590fd8b5a4c6e68098ad5625395";
+let currentSuggestions = [];
+
+searchInput.value = "";
+
+if (localStorage.getItem("mainCity")) {
+  createStructure();
+  fetchCurrentCity(localStorage.getItem("mainCity"));
 }
 
-function updateCurrentWeather(weather) {
-  const temp = Math.round(weather.main.temp);
-  const desc = weather.weather[0].description;
-  const icon = weather.weather[0].icon;
-  const name = weather.name;
-  const time = formatTime(weather.dt);
-
-  const tempElem = document.querySelectorAll('.temp-current');
-  const imgElem = document.querySelectorAll('.img-current');
-  const nameElem = document.querySelectorAll('.name-current');
-  const descElem = document.querySelectorAll('.desc-current');
-  const timeElem = document.querySelectorAll('.time-current');
-
-  tempElem.forEach((item) => {
-    item.textContent = `${temp}°C`;
-  });
-  nameElem.forEach((item) => {
-    item.textContent = name;
-  });
-  descElem.forEach((item) => {
-    item.textContent = desc;
-  });
-  timeElem.forEach((item) => {
-    item.textContent = time;
-  });
-  imgElem.forEach((item) => {
-    item.src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
-    item.alt = desc;
-  });
-}
-
-function fetchWeather(city) {
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${API_KEY}`;
-
-  fetch(apiUrl)
-    .then((response) => response.json())
-    .then((weather) => {
-      if (weather.cod === 200) {
-        updateCurrentWeather(weather);
-        console.log(weather);
-      } else {
-        console.error(`Fehler beim Abrufen der Daten: ${weather.message}`);
-      }
-    })
-    .catch((error) => {
-      console.error("API Fehler:", error);
-    });
-}
-
-function fetchForecast(city) {
-  const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&APPID=${API_KEY}`;
-
-  fetch(apiUrl)
-    .then((response) => response.json())
-    .then((forecast) => {
-      if (forecast.cod === "200") {
-        updateForecast(forecast);
-        updateWeatherForDays(forecast.list);
-        updateWeekdays();
-        console.log(forecast);
-      } else {
-        console.error(`Fehler beim Abrufen der Vorhersage: ${forecast.message}`);
-      }
-    })
-    .catch((error) => {
-      console.error("API Fehler:", error);
-    });
-}
-
-function updateForecast(forecast) {
-  const forecasts = forecast.list.slice(0, 5);
-
-  forecasts.forEach((item, index) => {
-    const temp = Math.round(item.main.temp);
-    const time = formatTime(item.dt);
-    const icon = item.weather[0].icon;
-
-    const imgElem = document.querySelector(`.img-current-${index + 1}`);
-    const tempElem = document.querySelector(`.temp-current-${index + 1}`);
-    const timeElem = document.querySelector(`.time-current-${index + 1}`);
-
-    if (imgElem && tempElem && timeElem) {
-      imgElem.src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
-      tempElem.textContent = `${temp}°C`;
-      timeElem.textContent = time;
-    }
-  });
-}
-
-function getWeekdays(amount) {
-  const weekdays = ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"];
-  const today = new Date();
-  const nextDays = [];
-
-  for (let i = 1; i <= amount; i++) {
-    const nextDay = new Date(today);
-    nextDay.setDate(today.getDate() + i);
-    nextDays.push(weekdays[nextDay.getDay()]);
+async function fetchCities(query) {
+  if (query.length < 2) {
+    suggestionsContainer.style.display = "none";
+    return;
   }
 
-  return nextDays;
+  const url = `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=7&appid=${apiKey}`;
+  try {
+    const response = await fetch(url);
+    currentSuggestions = await response.json();
+    showSuggestions(currentSuggestions);
+  } catch (error) {
+    console.error("Fehler beim Laden der Städte:", error);
+  }
 }
 
-function updateWeekdays() {
-  const days = getWeekdays(6);
+function showSuggestions(cities) {
+  suggestionsContainer.innerHTML = "";
+  if (cities.length === 0) {
+    suggestionsContainer.style.display = "none";
+    return;
+  }
 
-  days.forEach((day, index) => {
-    const dayElem = document.querySelector(`.day-${index + 1}`);
-    if (dayElem) {
-      dayElem.textContent = day;
-    }
-  });
-}
-
-function groupForecastByDay(forecastList) {
-  const grouped = {};
-
-  forecastList.forEach((item) => {
-    const date = new Date(item.dt * 1000).toISOString().split("T")[0];
-    if (!grouped[date]) {
-      grouped[date] = [];
-    }
-    grouped[date].push(item.main.temp);
-  });
-
-  return grouped;
-}
-
-function calculateDailyStats(groupedData) {
-  return Object.entries(groupedData).map(([date, temps]) => {
-    const sortedTemps = temps.sort((a, b) => a - b);
-    const min = Math.min(...temps);
-    const max = Math.max(...temps);
-    const median =
-      sortedTemps.length % 2 === 0
-        ? (sortedTemps[sortedTemps.length / 2 - 1] + sortedTemps[sortedTemps.length / 2]) / 2
-        : sortedTemps[Math.floor(sortedTemps.length / 2)];
-
-    return { date, min, max, median };
-  });
-}
-
-function updateWeatherForDays(forecastList) {
-  const groupedData = groupForecastByDay(forecastList);
-  const dailyStats = calculateDailyStats(groupedData);
-  const weekdays = getWeekdays(6);
-
-  dailyStats.slice(0, 6).forEach((stats, index) => {
-    const minElem = document.querySelector(`.min-temp-day-${index + 1}`);
-    const maxElem = document.querySelector(`.max-temp-day-${index + 1}`);
-    const medElem = document.querySelector(`.med-temp-day-${index + 1}`);
-    const dayElem = document.querySelector(`.day-${index + 1}`);
-
-    if (minElem && medElem && maxElem && dayElem) {
-      const offset = ((stats.median - stats.min) / (stats.max - stats.min)) * 100;
-
-      dayElem.textContent = weekdays[index];
-      minElem.textContent = `${Math.round(stats.min)}°C`;
-      maxElem.textContent = `${Math.round(stats.max)}°C`;
-
-      medElem.style.setProperty('--line-offset', `${offset}%`);
-    }
-  });
-}
-
-function fetchWeatherOtherCities(cityArray) {
-  cityArray.forEach((city, index) => {
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${API_KEY}`;
-
-    fetch(apiUrl)
-    .then((response) => response.json())
-    .then((weather) => {
-      if (weather.cod === 200) {
-        updateOtherCitiesCurrentWeather(weather, index + 1);
-        console.log(weather);
-      } else {
-        console.error(`Fehler beim Abrufen der Daten: ${weather.message}`);
-      }
-    })
-    .catch((error) => {
-      console.error("API Fehler:", error);
+  cities.forEach(city => {
+    const div = document.createElement("div");
+    div.textContent = `${city.name}, ${city.country}`;
+    div.addEventListener("click", () => {
+      selectCity(city.name);
     });
+    suggestionsContainer.appendChild(div);
   });
+
+  suggestionsContainer.style.display = "block";
 }
 
-function fetchForecastOtherCities(city) {
-  city.forEach((city, index) => {
-    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&APPID=${API_KEY}`;
-
-    fetch(apiUrl)
-    .then((response) => response.json())
-    .then((forecast) => {
-      if (forecast.cod === "200") {
-
-        updateOtherCitiesForecast(forecast, index + 1);
-
-        console.log(forecast);
-      } else {
-        console.error(`Fehler beim Abrufen der Vorhersage: ${forecast.message}`);
-      }
-    })
-    .catch((error) => {
-      console.error("API Fehler:", error);
-    });
-  });
+function selectCity(cityName) {
+  suggestionsContainer.style.display = "none";
+  localStorage.setItem("mainCity", cityName);
+  createStructure();
+  fetchCurrentCity(cityName);
 }
 
-function updateOtherCitiesForecast(forecast, id) {
-  const forecasts = forecast.list.slice(0, 2);
-  const weekdays = getWeekdays(2);
+searchInput.addEventListener("input", () => {
+  fetchCities(searchInput.value);
+});
 
-  forecasts.forEach((item, index) => {
-    const icon = item.weather[0].icon;
-    const desc = item.weather[0].description;
-    const maxTemp = Math.round(item.main.temp_max);
-    const minTemp = Math.round(item.main.temp_min);
-    const medTemp = Math.round((maxTemp + minTemp) / 2);
+searchInput.addEventListener("keydown", (event) => {
+  if (event.key === "Enter" && currentSuggestions.length > 0) {
+    event.preventDefault();
+    selectCity(currentSuggestions[0].name);
+  }
+});
 
-    const imgElem = document.querySelector(`.city-${id}-img-day-${index + 1}`);
-    const maxTempElem = document.querySelector(`.city-${id}-max-temp-day-${index + 1}`);
-    const medTempElem = document.querySelector(`.city-${id}-med-temp-day-${index + 1}`);
-    const minTempElem = document.querySelector(`.city-${id}-min-temp-day-${index + 1}`);
-    const dayElem = document.querySelector(`.city-${id}-day-${index + 1}`);
+document.addEventListener("click", (event) => {
+  if (!event.target.closest(".search-container")) {
+    suggestionsContainer.style.display = "none";
+  }
+});
 
-    const offset = ((medTemp - minTemp) / (maxTemp - minTemp)) * 100;
-    medTempElem.style.setProperty('--line-offset', `${offset}%`);
-
-    imgElem.src = `https://openweathermap.org/img/wn/${icon}.png`;
-    imgElem.alt = desc;
-    maxTempElem.textContent = `${maxTemp}°C`;
-    minTempElem.textContent = `${minTemp}°C`;
-    dayElem.textContent = weekdays[index];
-  });
-}
-
-function updateOtherCitiesCurrentWeather(weather, id) {
-  const temp = Math.round(weather.main.temp);
-  const icon = weather.weather[0].icon;
-  const name = weather.name;
-  const maxTemp = Math.round(weather.main.temp_max);
-  const minTemp = Math.round(weather.main.temp_min);
-  const medTemp = Math.round((maxTemp + minTemp) / 2);
-  const desc = weather.weather[0].description;
-
-  const tempElem = document.querySelector(`.city-${id}-temp-current`);
-  const imgElem = document.querySelector(`.city-${id}-img-current`);
-  const minTempElem = document.querySelector(`.city-${id}-min-temp-current`);
-  const medTempElem = document.querySelector(`.city-${id}-med-temp-current`);
-  const maxTempElem = document.querySelector(`.city-${id}-max-temp-current`);
-  const nameElem = document.querySelector(`.city-${id}`);
-  const todayElem = document.querySelector(`.city-${id}-day-0`);
-
-  const offset = ((medTemp - minTemp) / (maxTemp - minTemp)) * 100;
-  medTempElem.style.setProperty('--line-offset', `${offset}%`);
-
-  tempElem.textContent = `${temp}°C`;
-  nameElem.textContent = name;
-  minTempElem.textContent = `${minTemp}°C`;
-  maxTempElem.textContent = `${maxTemp}°C`;
-  todayElem.textContent = "Heute";
-
-  imgElem.src = `https://openweathermap.org/img/wn/${icon}.png`;
-  imgElem.alt = desc;
-}
-
-function fetchOtherCities(cityArray) {
-  fetchWeatherOtherCities(cityArray);
-  fetchForecastOtherCities(cityArray);
-}
-
-function fetchCurrentCity(city) {
-  fetchWeather(city);
-  fetchForecast(city);
-}
-
-const cityArray = ["Rom", "Washington", "Athen"];
-
-fetchCurrentCity("Tokyo");
-fetchOtherCities(cityArray);
+document.addEventListener('keydown', function(event) {
+    if (event.ctrlKey && event.key === 'k') {
+        event.preventDefault();
+        document.querySelector('.search-main').focus();
+    }
+});
 
